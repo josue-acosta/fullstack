3974f924c034ce0b3f1110eccfdcb1a9bfd7203c
josue-acosta <hannibalsway001@gmail.com>
2020-10-27 00:59:34 -0500

    phase 1 of google authentication

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express')
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
+
+// app configure
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ 'Message': 'Success!!!' });
-});
+// passport OAuth config
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: keys.googleCallbackURL
+        },
+        (accessToken, refreshToken, profile, done) => {
+            console.log(`accessToken: ${accessToken}`)
+            console.log(`refreshToken: ${refreshToken}`)
+            console.log(profile)
+        }
+    )
+);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT)
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+
+
+
+
+// run app
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
