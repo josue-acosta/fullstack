1ac025c7f38cfccfe2aff658ae48bf20a92f20a3
josue-acosta <hannibalsway001@gmail.com>
2020-10-28 18:51:09 -0500

    dashboard navigation added

--- client/src/components/dashboard/listItems.js
+++ client/src/components/dashboard/listItems.js
@@ -27,22 +27,22 @@ export const mainListItems = (
             <ListItemText primary="Dashboard" />
         </ListItem>
 
-        <ListItem button component={NavLink} to="/new-order" >
+        <ListItem button component={NavLink} to="/dashboard/new-order" >
             <ListItemIcon><AddIcon /></ListItemIcon>
             <ListItemText primary="New Order" />
         </ListItem>
 
-        <ListItem button component={NavLink} to="/view-orders" >
+        <ListItem button component={NavLink} to="/dashboard/view-orders" >
             <ListItemIcon><ViewListIcon /></ListItemIcon>
             <ListItemText primary="View Orders" />
         </ListItem>
 
-        <ListItem button component={NavLink} to="/gross-sales" >
+        <ListItem button component={NavLink} to="/dashboard/gross-sales" >
             <ListItemIcon><ReceiptIcon /></ListItemIcon>
             <ListItemText primary="Gross Sales" />
         </ListItem>
 
-        <ListItem button component={NavLink} to="/food-cost" >
+        <ListItem button component={NavLink} to="/dashboard/food-cost" >
             <ListItemIcon><AddShoppingCartIcon /></ListItemIcon>
             <ListItemText primary="Food Cost" />
         </ListItem>
@@ -51,27 +51,27 @@ export const mainListItems = (
 
 export const terciaryListItems = (
     <div>
-        <ListItem button component={NavLink} to="/revenue-streams" >
+        <ListItem button component={NavLink} to="//dashboardrevenue-streams" >
             <ListItemIcon><DonutLargeIcon /></ListItemIcon>
             <ListItemText primary="Revenue Streams" />
         </ListItem>
 
-        <ListItem button component={NavLink} to="/break-even-point" >
+        <ListItem button component={NavLink} to="/dashboard/break-even-point" >
             <ListItemIcon><MultilineChartIcon /></ListItemIcon>
             <ListItemText primary="Break Even Point" />
         </ListItem>
 
-        <ListItem button component={NavLink} to="/balance-sheet" >
+        <ListItem button component={NavLink} to="/dashboard/balance-sheet" >
             <ListItemIcon><DescriptionIcon /></ListItemIcon>
             <ListItemText primary="Balance Sheet" />
         </ListItem>
 
-        <ListItem button component={NavLink} to="/profit-and-loss" >
+        <ListItem button component={NavLink} to="/dashboard/profit-and-loss" >
             <ListItemIcon><ExposureIcon style={{ transform: 'rotate(180deg)' }} /></ListItemIcon>
             <ListItemText primary="Profit &amp; Loss" />
         </ListItem>
 
-        <ListItem button component={NavLink} to="/cash-flow" >
+        <ListItem button component={NavLink} to="/dashboard/cash-flow" >
             <ListItemIcon><TrendingUpIcon /></ListItemIcon>
             <ListItemText primary="Cash Flow" />
         </ListItem>
