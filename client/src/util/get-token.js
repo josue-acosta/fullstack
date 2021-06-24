79cd6c0f94135adb3374eadb5a67aa664227effd
josue-acosta <hannibalsway001@gmail.com>
2021-06-24 13:10:59 -0500

    find accountSid 01

--- client/src/util/get-token.js
+++ client/src/util/get-token.js
@@ -1,9 +1,14 @@
 const keys = require('../config/keys')
-const twilioAccountSid = keys.twilioAccountSid;
-const twilioApiKey = keys.twilioApiKey;
-const twilioApiSecret = keys.twilioApiSecret;
-const serviceSid = keys.serviceSid;
-const identity = keys.identity;
+// const twilioAccountSid = keys.twilioAccountSid;
+// const twilioApiKey = keys.twilioApiKey;
+// const twilioApiSecret = keys.twilioApiSecret;
+// const serviceSid = keys.serviceSid;
+// const identity = keys.identity;
+const twilioAccountSid = 'ACdf6e48b8b0afb9802ff6446d2cb03acb';
+const twilioApiKey = 'SK35d747f12f534d9248456812d9db149d';
+const twilioApiSecret = '92EJm42U6o8TJ7pkBWiFdWeBGUBxz8pS';
+const serviceSid = 'ISea84a3d4463f46f19fafa360e4cea195';
+const identity = 'testPineapple';
 const AccessToken = require('twilio').jwt.AccessToken;
 const ChatGrant = AccessToken.ChatGrant;
 
@@ -19,7 +24,9 @@ module.exports = {
 
             // Create an access token which we will sign and return to the client,
             // containing the grant we just created
-            console.log(twilioAccountSid)
+            console.log(keys)
+            console.log(`keys.twilioAccountSid: ${keys.twilioAccountSid}`)
+            console.log(`twilioAccountSid: ${twilioAccountSid}`)
             const token = new AccessToken(
                 twilioAccountSid,
                 twilioApiKey,
